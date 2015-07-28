4cfb017d14c8f3da5f76f48b21b3ae47b985fcec
Szczyp <qb@szczyp.com>
2015-07-28 04:29:54 +0000

    Deploy 1 now without out!

--- out/boot/cljs/main.js
+++
@@ -1,4 +0,0 @@
-// Compiled by ClojureScript 0.0-3308 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main');
-goog.require('cljs.core');
-goog.require('pupa.core');
--- out/jamesmacaulay/zelkova/impl/time.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 0.0-3308 {:static-fns true, :optimize-constants true}
-goog.provide('jamesmacaulay.zelkova.impl.time');
-goog.require('cljs.core');
-jamesmacaulay.zelkova.impl.time.now = (function jamesmacaulay$zelkova$impl$time$now(){
-return (new Date()).valueOf();
-});
